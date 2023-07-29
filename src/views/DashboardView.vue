a07a057d3aa361a2bba4f9810cd1c1610edb938e
chrisluo5311 <chrislo5311@gmail.com>
2023-07-30 02:36:04 +0800

    update product list

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,20 +1,29 @@
 <template>
-    Dashboard
+  <Navbar></Navbar>
+  <div class="container-fluid">
+    <router-view />
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/NavBar.vue'
+
 export default {
+  components: {
+    Navbar
+  },
   created () {
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1')
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1'
+    )
     console.log(token)
     this.$http.defaults.headers.common.Authorization = token
     const api = `${process.env.VUE_APP_API}api/user/check`
-    this.$http.post(api)
-      .then((res) => {
-        if (!res.data.success) {
-          this.$router.push('/login')
-        }
-      })
+    this.$http.post(api).then((res) => {
+      if (!res.data.success) {
+        this.$router.push('/login')
+      }
+    })
   }
 }
 </script>
