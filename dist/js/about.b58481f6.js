ab1eb7671924dbbbccce745beef309e193f86215
chrisluo5311 <chrislo5311@gmail.com>
2023-08-10 23:57:41 +0800

    add dist folder

---
+++ dist/js/about.b58481f6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_final_project"]=self["webpackChunkvue_final_project"]||[]).push([[443],{833:function(n,e,u){u.r(e),u.d(e,{default:function(){return l}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),c=[r];function s(n,e){return(0,t.wg)(),(0,t.iD)("div",a,c)}var i=u(89);const o={},f=(0,i.Z)(o,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.b58481f6.js.map
